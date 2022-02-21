78cbd8199a4c6a28463396d2efa6b135574820ff
vscheff <98859370+vscheff@users.noreply.github.com>
2022-02-21 14:56:50 -0500

    Code Refectored & Commented
    All files have now been commented. In doing so, some code has been refactored, modified, and/or removed.

--- Cogs/Classes.py
+++ Cogs/Classes.py
@@ -1,71 +1,86 @@
+# Cog that holds all commands related to WMU classes & majors
+
 from discord.ext import commands
-from dotenv import load_dotenv
 from re import search, sub
-from os import getenv
-from majors import major_abbrev
 import discord
 
-load_dotenv
-GUILD = getenv('DISCORD_GUILD')
+# Local dependencies
+from majors import major_abbrev
 
 
 class Classes(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+
+    # attr bot - our client
+    # attr guild - target Discord server
+    # major_list - list of valid majors
+    def __init__(self, bot: commands.Bot, guild: discord.Guild):
         self.bot = bot
+        self.guild = guild
         self.major_list = []
         with open('message.txt', 'r') as inFile:
-            for line in inFile:
-                self.major_list.append(line.strip().lower())
+            self.major_list = [line.strip().lower() for line in inFile]
 
+    # $major command used to assign users to the role representing their major
+    # param major - all user input following command-name
     @commands.command(help='Select your major as a server role.\nExample: $major Computer Science\n\n'
                            'To view a list of supported majors, '
                            'use the command with 0 arguments\nExample: $major\n\n'
                            'For some majors you may also use their abbreviation code\nExample: $major AAAS',
                       brief='Select your major as a server role.')
     async def major(self, ctx, *major):
-        major = sub(r"'", '', ' '.join(major).lower())
+        # Lower and join user input, and remove all apostrophes
+        major = sub(r"'", '', ' '.join(major)).lower()
         if major == 'help':
             await ctx.send(self.major.help)
             return
+        # Check if user input is in our abbreviations mapping
         if major in major_abbrev:
             major = major_abbrev[major].lower()
         elif major not in self.major_list:
+            # Only warn the user about bad input if they included an argument that wasn't "list"
             if major and major != 'list':
                 await ctx.send('Invalid major! Please be sure to type your major exactly as it appears.\n')
             await ctx.send(content='Supported majors include:',
                            file=discord.File('./message.txt', filename='Majors.txt'))
             return
-        guild = discord.utils.get(self.bot.guilds, name=GUILD)
-        roles = await guild.fetch_roles()
-        selected_role = [i for i in roles if i.name.lower() == major][0]
+        # Find the role requested by the user
+        selected_role = discord.utils.find(lambda x: x.name.lower() == major, self.guild.roles)
         await ctx.author.add_roles(selected_role)
         await ctx.send(f'Success! You are now a member of {selected_role.name}!')
 
+    # Used by $join and $leave to validate user input for class names
+    # param class_name - one instance of user input for class name
     async def validate_class(self, ctx, class_name):
+        # Pull non-digit characters from the beginning of the argument
         match = search(r'\A\D+', class_name)
         if match is None:
-            await ctx.send(f'Unrecognized course name: {class_name}\n'
-                           f'Please include a valid department prefix.')
+            await ctx.send(f'Unrecognized course name: {class_name}\nPlease include a valid department prefix.')
             return
         else:
             department = match.group().lower()
-            class_number = class_name[len(department):]
+            # Pull the rest of the argument that was not matched
+            class_number = class_name[match.span()[1]:]
+
         try:
             class_int = int(class_number)
+            # Class numbers at WMU are strictly in this range
             if class_int < 1000 or class_int > 8000:
                 print(f'Bad range for class number: {class_int}')
-                await ctx.send(f'Invalid class number: {class_number}')
+                await ctx.send(f'Invalid class number: {class_name}')
                 return
         except ValueError:
             print(f'Caught ValueError on class number: {class_number}')
-            await ctx.send(f'Invalid class number: {class_number}')
+            await ctx.send(f'Invalid class number: {class_name}')
             return
         dep_length = len(department)
-        if dep_length > 4 or dep_length < 2:
+        # Department names are strictly 2, 3, or 4 letters
+        if not 1 < dep_length < 5:
             await ctx.send(f'Invalid department prefix: {department}')
             return
         return department, class_number
 
+    # $join command used to add users to text channels for classes
+    # param arg - all user input following command-name
     @commands.command(help='Join the text channel for a specified class.\nExample: $join CS1120\n\n'
                            'You may also join several classes at once by using a comma-separated list.\n'
                            'Example: $join cs1120, MATH 2240, phys-2130',
@@ -75,40 +90,37 @@ class Classes(commands.Cog):
         if arg == 'help':
             await ctx.send(self.join.help)
             return
-        guild = discord.utils.get(self.bot.guilds, name=GUILD)
         successful_joins = []
         for element in arg.split(','):
+            # Remove all whitespace and hyphens from the argument
             class_name = sub(r'[\s\-]', '', element)
             class_info = await self.validate_class(ctx, class_name)
             if class_info is None:
                 await ctx.send(f'Course "{class_name}" not joined.')
                 continue
             department, class_number = class_info
-            category = [i for i in guild.categories if i.name == department]
-            overwrites = {
-                guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            }
-            if len(category) == 0:
+            # Get the category for the department of this class
+            category = discord.utils.get(self.guild.categories, name=department)
+            # Used when creating categories and channels to hide them from users by default
+            o_writes = {self.guild.default_role: discord.PermissionOverwrite(read_messages=False)}
+            if category is None:
                 print(f'Creating category {department}')
-                category = await guild.create_category(department, overwrites=overwrites)
-            else:
-                category = category[0]
+                category = await self.guild.create_category(department, overwrites=o_writes)
             class_name = department + class_number
-            channel = [i for i in category.text_channels if i.name == class_name]
-            if len(channel) > 0:
+            # Get the channel for this class
+            channel = discord.utils.get(category.text_channels, name=class_name)
+            if channel:
                 print(f'Adding user {ctx.author.name} to channel {class_name}')
-                await channel[0].set_permissions(ctx.author, read_messages=True)
+                await channel.set_permissions(ctx.author, read_messages=True)
             else:
                 print(f'Creating channel {class_name}')
-                channel = await guild.create_text_channel(class_name,
-                                                          overwrites=overwrites,
-                                                          category=category)
+                channel = await self.guild.create_text_channel(class_name, overwrites=o_writes, category=category)
                 await channel.set_permissions(ctx.author, read_messages=True)
             successful_joins.append(class_name)
         if successful_joins:
             await ctx.send(f'Successfully joined text channels for: {", ".join(successful_joins)}')
 
-
+    # Called if $join encounters an unhandled exception
     @join.error
     async def join_error(self, ctx, error):
         if isinstance(error, commands.errors.MissingRequiredArgument):
@@ -118,6 +130,8 @@ class Classes(commands.Cog):
         else:
             print(f'$join command failed with error:\n\n{error}')
 
+    # $leave command used by users to leave class-specific channels
+    # param arg - all user input following command-name
     @commands.command(help='Leave the text channel for a specified class.\nExample: $leave CS1120\n\n'
                            'You may also leave several classes at once by using a comma-separated list.\n'
                            'Example: $leave cs1120, math 2240, PHYS-2130\n\n'
@@ -131,20 +145,23 @@ class Classes(commands.Cog):
         if arg == 'all':
             await self.leave_all(ctx)
             return
-        guild = discord.utils.get(self.bot.guilds, name=GUILD)
         successful_leaves = []
         for element in arg.split(','):
+            # Remove all whitespace and hyphens from the argument
             class_name = sub(r'[\s\-]', '', element)
             class_info = await self.validate_class(ctx, class_name)
             if class_info is None:
                 await ctx.send(f'Course "{class_name}" not left.')
                 continue
             department, class_number = class_info
-            category = [i for i in guild.categories if i.name == department]
-            if len(category) > 0:
-                channel = [i for i in category[0].text_channels if i.name == class_name]
-                if len(channel) > 0:
-                    await channel[0].set_permissions(ctx.author, read_messages=False)
+            # Get the category for the department of this class
+            category = discord.utils.get(self.guild.categories, name=department)
+            if category:
+                # Get the channel for this class
+                channel = discord.utils.get(category.text_channels, name=class_name)
+                if channel:
+                    # Hide this channel from the user who sent the command
+                    await channel.set_permissions(ctx.author, read_messages=None)
                     successful_leaves.append(class_name)
                 else:
                     await ctx.send(f'No channel with name "{class_name}" found, channel not left.')
@@ -153,6 +170,7 @@ class Classes(commands.Cog):
         if successful_leaves:
             await ctx.send(f'Successfully left text channels for: {", ".join(successful_leaves)}')
 
+    # Called if $leave encounters an unhandled exception
     @leave.error
     async def leave_error(self, ctx, error):
         if isinstance(error, commands.errors.MissingRequiredArgument):
@@ -162,26 +180,31 @@ class Classes(commands.Cog):
         else:
             print(f'$leave command failed with error:\n\n{error}')
 
+    # Called by $leave if user passes "all" as an argument
+    # Removes user from all class-specific channels for which they are a member
+    # param ctx - message context forwarded from $leave
     async def leave_all(self, ctx):
-        guild = discord.utils.get(self.bot.guilds, name=GUILD)
         channels_left = []
-        for channel in guild.text_channels:
-            if not channel.overwrites_for(ctx.author).is_empty():
-                await channel.set_permissions(ctx.author, overwrite=None)
-                channels_left.append(channel.name)
+        # Loop once for each text channel for which the user has permission overwrite(s)
+        for channel in filter(lambda x: not x.overwrites_for(ctx.author).is_empty(), self.guild.text_channels):
+            # Clear all of the user's overwrites for this channel
+            await channel.set_permissions(ctx.author, overwrite=None)
+            channels_left.append(channel.name)
         if channels_left:
             await ctx.send(f'Successfully left text channels for: {", ".join(channels_left)}')
         else:
             await ctx.send("No text channels left. Are you sure you're a member of any?")
 
+    # Hidden $add_roles command used to bulk add a collection of roles
+    # param filename - string representing the filepath for role definition file
     @commands.command(hidden=True)
     @commands.has_permissions(manage_roles=True)
     async def add_roles(self, ctx, filename):
-        guild = discord.utils.get(self.bot.guilds, name=GUILD)
         try:
             with open(filename, 'r') as inFile:
-                role_objects = await guild.fetch_roles()
-                role_names = [i.name for i in role_objects]
+                # A list of the names of all existing roles
+                role_names = [i.name for i in self.guild.roles]
+                # This will be the role color of all added roles
                 color = discord.Colour.orange()
                 count_success = 0
                 count_failure = 0
@@ -189,18 +212,19 @@ class Classes(commands.Cog):
                 for line in inFile:
                     name = line.strip()
                     if name not in role_names:
-                        print(f'Creating role: {name}')
-                        await guild.create_role(name=name, colour=color)
+                        print(f'Creating role {count_success}: {name}')
+                        await self.guild.create_role(name=name, colour=color)
                         count_success += 1
                     else:
                         print('Skipping redundant role: {name}')
                         count_failure += 1
-                await ctx.send(f'Operation complete\n' \
-                               f'Total Roles Added: {count_success}\n' \
+                await ctx.send(f'Operation complete\n'
+                               f'Total Roles Added: {count_success}\n'
                                f'Redundant Roles Skipped: {count_failure}')
         except FileNotFoundError:
             await ctx.send(f'No file in directory found with name: {filename}')
 
+    # Called if $add_roles encounters an unhandled exception
     @add_roles.error
     async def add_roles_error(self, ctx, error):
         if isinstance(error, commands.errors.MissingPermissions):
--- Cogs/LoopTasks.py
+++ Cogs/LoopTasks.py
@@ -1,42 +1,43 @@
+# Cog that holds all looping tasks for the bot
+
 from discord.ext import commands, tasks
 from os import getenv
-from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import discord
 
-load_dotenv
-GUILD = getenv('DISCORD_GUILD')
-WELCOME_CHANNEL_ID = int(getenv('WELCOME_CH_ID'))
-ROLE_CLAIM_CHANNEL_ID = int(getenv('ROLECLM_CH_ID'))
+WELCOME_CHANNEL_ID = int(getenv('WELCOME_CH_ID'))     # ID of channel 'welcome'
+ROLE_CLAIM_CHANNEL_ID = int(getenv('ROLECLM_CH_ID'))  # ID of channel 'claim-your-major-and-classes'
 
 
 class LoopTasks(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+
+    # attr bot - our client
+    # attr guild - target Discord server
+    def __init__(self, bot: commands.Bot, guild: discord.Guild):
         self.bot = bot
-        self.guild = None
+        self.guild = guild
         self.ch_welcome = None
         self.ch_roleClaim = None
-        
+
         self.purge_channels.start()
-    
+
+    # Purges general channels of messages older than 3 days once a day
     @tasks.loop(hours=24)
     async def purge_channels(self):
         print(f'Beginning default channel purge #{self.purge_channels.current_loop}:')
         del_before = datetime.now() - timedelta(hours=72)
         print(f'Deleting messages before {del_before}')
         for channel in (self.ch_welcome, self.ch_roleClaim):
-            await self.delete_messages(channel, del_before)
+            # Delete all messages sent before del_before
+            deleted = await channel.purge(before=del_before)
+            print(f'Deleted {len(deleted)} messages from {channel.name}')
         print('Purge successfully completed!\n')
-    
-    async def delete_messages(self, channel, del_before):
-        deleted = await channel.purge(before=del_before)
-        print(f'Deleted {len(deleted)} messages from {channel.name}')
-        
+
+    # Called before the purge_channels loop starts
     @purge_channels.before_loop
     async def before_purge(self):
-        await self.bot.wait_until_ready()        
-        print('Starting default channel purge 24 hour loop')        
-        self.guild = discord.utils.get(self.bot.guilds, name=GUILD)
+        await self.bot.wait_until_ready()
+        print('Starting default channel purge 24 hour loop')
         self.ch_welcome = self.guild.get_channel(WELCOME_CHANNEL_ID)
         if self.ch_welcome is None:
             print('WARNING: Welcome channel not found. Loop not started.\n')
@@ -48,4 +49,3 @@ class LoopTasks(commands.Cog):
             self.purge_channels.cancel()
             return
         print('Loop successfully started!\n')
-
