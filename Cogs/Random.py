9ff25df67db1294dd789f6bf9747baec0fb4667f
vscheff <vonscheffler@protonmail.com>
2022-09-01 09:58:44 -0400

    Small roll change

--- Cogs/Random.py
+++ Cogs/Random.py
@@ -5,7 +5,7 @@ from random import choice, randint
 
 from msg_packager import package_message
 
-MAX_ROLL = 2 ** 18 - 1
+MAX_ROLL = 2 ** 18
 
 
 class Random(commands.Cog):
@@ -79,7 +79,7 @@ class Random(commands.Cog):
         if quantity < 1 or size < 1:
             await ctx.send('Please use only positive integers for dice quantity and number of sides.')
             return
-        if quantity > MAX_ROLL or size > MAX_ROLL:
+        if quantity >= MAX_ROLL or size >= MAX_ROLL:
             await ctx.send(f'Please only use integers smaller than {MAX_ROLL}.')
             return
         roll_list = []
